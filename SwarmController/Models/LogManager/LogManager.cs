be936e809f3002307a1c69ded39a47cebd68c86b
fmurat17 <fatihce23otmail.com>
2022-06-12 15:34:29 +0300

    log dosyası adı değiştirildi

---
+++ SwarmController/Models/LogManager/LogManager.cs
@@ -0,0 +1,45 @@
+﻿using SwarmController.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SwarmController.Models.Log
+{
+    public class LogManager
+    {
+        private static LogManager logManager = null;
+        public static LogManager getLogManager()
+        {
+            if (logManager == null)
+            {
+                logManager = new LogManager();
+            }
+
+            return logManager;
+        }
+
+        public Queue<string> logList = new Queue<string>();
+
+        public void addLog(string log)
+        {
+            //(Application.Current.MainWindow as MainWindow).addLog(log);
+            logList.Enqueue(getCurrentTime() + " " + log);
+        }
+
+        public string getCurrentTime()
+        {
+            // Use current time, with a format string.
+            //DateTime time = DateTime.Now;
+            //string format = "MMM ddd d HH:mm yyyy";
+            //return time.ToString(format);
+
+            return DateTime.Now.ToString();
+
+        }
+
+    }
+}
